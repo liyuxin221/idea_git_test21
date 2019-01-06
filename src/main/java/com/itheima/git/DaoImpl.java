ef78964c619618fdfe8ca6ab80ff5b19bbdca575
liyuxin221 <liyuxin221@qq.com>
2019-01-06 19:46:31 +0800

    add DaoImpl.java

---
+++ src/main/java/com/itheima/git/DaoImpl.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+/**
+ * @author: Liyuxin wechat:13011800146.
+ * @date: 2019/1/6
+ * @description: 创建时间
+ */
+public class DaoImpl {
+    public void createTime() {
+        System.out.println("创建时间.");
+    }
+}
