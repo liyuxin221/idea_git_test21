94480c62af690da0d3aaaa9787211a93fa743006
liyuxin221 <liyuxin221@qq.com>
2019-01-06 19:23:04 +0800

    create GitTest.java

---
+++ src/main/java/com/itheima/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+import org.junit.Test;
+
+/**
+ * @author: Liyuxin wechat:13011800146.
+ * @date: 2019/1/6
+ * @description: create GitTest.java
+ */
+public class GitTest {
+
+    @Test
+    public void test1() {
+        System.out.println("This is a git test project.");
+    }
+}
